0d74abb0e858b4e35d7745a05ffcd6c9b68f9e49
Madhan <mdsignz@gmail.com>
2019-02-12 17:15:19 +0100

    Updates

---
+++ precache-manifest.a915576d7e947f94e6057e12c44880b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d8e934f629550310d9684299f763e52",
+    "url": "/github-issues/static/media/bg.6d8e934f.png"
+  },
+  {
+    "revision": "e36af9f4c3a30c8162bc",
+    "url": "/github-issues/static/js/runtime~main.e36af9f4.js"
+  },
+  {
+    "revision": "3eeef48b2926aeebbde0",
+    "url": "/github-issues/static/js/main.3eeef48b.chunk.js"
+  },
+  {
+    "revision": "e32469b4ea0c02bf2675",
+    "url": "/github-issues/static/js/1.e32469b4.chunk.js"
+  },
+  {
+    "revision": "3eeef48b2926aeebbde0",
+    "url": "/github-issues/static/css/main.d5c3a487.chunk.css"
+  },
+  {
+    "revision": "8468d2b2de27c72c1c2bba0f48a635b2",
+    "url": "/github-issues/index.html"
+  }
+];
